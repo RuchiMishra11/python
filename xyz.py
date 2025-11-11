f22d06b2d7e6b20e1c48d272802b7d6a40cd3317
RuchiMishra11 <ruchiimishra1227@gmail.com>
2025-11-11 15:27:05 +0530

    Updated files

--- xyz.py
+++
@@ -1,5 +0,0 @@
-password=input("Enter your password: ")
-while not (len(password)>8 and password.isalnum() and any(ch.upper() for ch in password)):
-    print("Your password is invalid.")
-    password=input("Enter your password again : ")
-print("wellcome!")
